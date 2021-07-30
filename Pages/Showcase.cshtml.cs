f43435b784ecdc765530ac798518ad5b1e3c93b5
Greg <gregoryonbusiness@gmail.com>
2021-07-30 19:05:40 +1000

    Init Commit for ASP.NET Server

---
+++ Pages/Showcase.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ComplexWeb.Pages
+{
+    public class ShowcaseModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        // Brings connection status from AppConnect Class forward to use in Razor Page
+        public bool p_isConnected { get{ return AppConnect.p_hasConnected; } }
+
+        public ShowcaseModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            System.Diagnostics.Debug.WriteLine( "GET USED" );
+        }
+
+        public void OnPost()
+        {
+            System.Diagnostics.Debug.WriteLine( "POST USED" );
+        }
+
+        public void OnPostSounder( string l_soundName )
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ComplexWeb
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            AppConnect.StartListenAsync();
+
+            // For normal Website
+            CreateHostBuilder(args).Build().Run();
+
+        }
+
+        // For website
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+    // Static cause I am lazy
+    public class AppConnect
+    {
+        // The server that will listen
+        static TcpListener s_server;
+
+        // The information for a single client that will connect
+        static TcpClient s_client;
+
+        // The thread to run async tasks
+        static Thread s_thread;
+
+        // Acts as a mutex, so the program will only act on a connction that is fully established
+        static private bool s_hasConnected = false;
+        static public bool p_hasConnected{ get{ return s_hasConnected;} }
+
+        // Used for checking disconnect
+        static private bool s_stillConnected = false;
+        
+        public static void StartListenAsync()
+        {
+            // Listening on TCP port 8888
+            s_server = new TcpListener( System.Net.IPAddress.Any, 8888);
+
+            // Start the server
+            s_server.Start();
+            Console.WriteLine( "Server started. Waiting for connection..." );
+
+            // Listen for Clients Async by doing on a new thread
+            Thread s_thread = new Thread( new ThreadStart( ListenAndWait ) );
+
+            // Name it and start it!
+            s_thread.Name = "GregAsync";
+            s_thread.Start();
+
+        }
+
+        // Meant to be run async
+        // First allows the client to connect and then keeps listening for messages
+        private static void ListenAndWait()
+        {
+            Console.WriteLine( "LISTENING YO" );
+
+            // This is blocking, which is why it will be performed in a seperate thread
+            s_client = s_server.AcceptTcpClient();
+
+            // Has connected!
+            s_hasConnected = true;
+            s_stillConnected = true;
+
+            while ( s_stillConnected )
+            {
+                // Checks if new data is available to be read from the network stream
+                if( s_client.Available > 0 )
+                {
+                    // Create an array large enough to hold the recieved data
+                    byte[] l_bytesRead = new byte[s_client.Available];
+
+                    // Read the data from the network stream
+                    s_client.GetStream().Read( l_bytesRead, 0, s_client.Available );
+
+                    // Convert the byte array to a string we can understand
+                    String l_str = System.Text.Encoding.ASCII.GetString( l_bytesRead );
+
+                    // For now, just write whatever it is to console
+                    Console.Write( l_str );
+                }
+
+                // Suspend this whole thread for 50ms
+                // We don't want to overload the server checking a quadrillion times a second
+                Thread.Sleep( 50 );
+            }
+        }
+    }
+}
